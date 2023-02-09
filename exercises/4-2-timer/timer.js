4c04d777eb40102f899a0d89e8a2f66bb70cda12
ALogachev <aelog16@yandex.ru>
2023-02-09 18:19:51 +0300

    feature/calculator Rewrite timer.js code

--- exercises/4-2-timer/timer.js
+++ exercises/4-2-timer/timer.js
@@ -1,24 +1,48 @@
-try {
-    let [timeStr, hours, minutes, seconds] = process.argv[2].match(/(\d+)?h\s+(\d+)?m\s+(\d+)?s/);
+import Emmiter from 'events';
 
-    if (
-        Number(minutes) > 60
-        || Number(seconds) > 60
-    ) {
-        throw new Error('Ошибка ввода: Число минут или секунд не может превышать 60.');
+const emmiter = new Emmiter();
+
+emmiter.on('error', (e) => {
+    console.log(e.message);
+
+    throw e;
+});
+
+/**
+ * Извлекает параметры из строки, переданной в таймер
+ *
+ * @param data - строка в формате "1h 4m 2s"
+ * @returns {[string,string,string]}
+ */
+const resolveParams = (data) => {
+    // Проверка входного типа параметра.
+    if (typeof data !== 'string') {
+        throw new TypeError('Ошибка ввода: Время таймера необходимо передать в формате строки.');
     }
-    console.log('BeforeSUM:', Number(hours), Number(minutes), Number(seconds));
-    console.log('SUM:', Number(hours) * 60 * 60 * 1000 + Number(minutes) * 60 * 1000 + Number(seconds) * 1000);
 
-    setTimeout(
-        () => console.log('Дзынь, дзынь! Таймер сработал!'),
-        Number(hours) * 60 * 60 * 1000 + Number(minutes) * 60 * 1000 + Number(seconds) * 1000
-    );
+    try {
+        let [timeStr, hours, minutes, seconds] = data.match(/(\d+)?h\s+(\d+)?m\s+(\d+)?s/);
 
-} catch (e) {
-    (e instanceof TypeError)
-        ?
-        console.log('Некорректный формат ввода таймера. Необходимо передать строку формата 1h 4m 2s.')
-        :
-        console.log(e.message);
+        return [hours, minutes, seconds];
+    } catch (e) {
+        emmiter.emit(
+            'error',
+            new Error('Ошибка ввода: Некорректный формат ввода таймера. Необходимо передать строку формата 1h 4m 2s.')
+        );
+    }
 }
+
+let [hours, minutes, seconds] = resolveParams(process.argv[2]);
+
+if (
+    Number(minutes) > 60
+    || Number(seconds) > 60
+) {
+    // throw new Error('Ошибка ввода: Число минут или секунд не может превышать 60.');
+    emmiter.emit('error', new Error('Ошибка ввода: Число минут или секунд не может превышать 60.'));
+}
+
+setTimeout(
+    () => console.log('Дзынь, дзынь! Таймер сработал!'),
+    Number(hours) * 60 * 60 * 1000 + Number(minutes) * 60 * 1000 + Number(seconds) * 1000
+);
