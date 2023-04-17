8b353a89c44a9b2a82bafedb8e4bc6f79c0115f0
ALogachev <aelog16@yandex.ru>
2023-04-17 22:48:59 +0300

    feature/TypeScriptArch Logger service was added

--- exercises/10-weather-typescript/index.ts
+++ exercises/10-weather-typescript/index.ts
@@ -1,10 +1,11 @@
 import { App } from "./src/app";
+import { LoggerService } from "./src/logger/logger.service";
 
 /**
  * Функция запуска приложения.
  */
 async function bootstrap() {
-    const app = new App();
+    const app = new App(new LoggerService());
     await app.init();
 }
 
--- exercises/10-weather-typescript/src/app.ts
+++ exercises/10-weather-typescript/src/app.ts
@@ -1,21 +1,24 @@
 import express, { Express }  from "express";
 import { Server } from 'http';
 import { userRouter } from "./routes/users/users";
+import { LoggerService } from "./logger/logger.service";
 
 export class App {
     public app: Express;
     public port: number;
     public server: Server;
+    private logger: LoggerService
 
-    constructor() {
+    constructor(logger: LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
     }
 
     public async init() {
         this.useRoutes();
         this.server = this.app.listen(this.port);
-        console.log(`Сервер запущен на http://localhost:${this.port}`);
+        this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
     }
 
     public useRoutes() {
