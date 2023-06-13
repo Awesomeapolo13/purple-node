cf4e2676c315b26cf9094e49f2b22364469ad6b9
ALogachev <aelof@yandex.ru>
2023-06-13 16:17:29 +0300

    Feature/TypeScriptArch The validation messages resetting in the validation middleware has been done

---
+++ exercises/10-weather-typescript/src/common/error/validation.error.code.enum.ts
@@ -0,0 +1,3 @@
+export enum ValidationErrorCodeEnum {
+	LANG_IS_EMPTY_CODE = 'langIsEmptyMsg',
+}
--- exercises/10-weather-typescript/src/common/middleware/validate.middleware.ts
+++ exercises/10-weather-typescript/src/common/middleware/validate.middleware.ts
@@ -1,19 +1,34 @@
 import { MiddlewareInterface } from './middleware.interface';
 import { Request, NextFunction, Response } from 'express';
 import { ClassConstructor, plainToInstance } from 'class-transformer';
-import { validate } from 'class-validator';
+import { validate, ValidationError } from 'class-validator';
+import { StorageServiceInterface } from '../../service/storage/storage.service.interface';
+import { AllowedTokenEnum } from '../../service/storage/allowed.token.enum';
+import { ValidationErrorCodeEnum } from '../error/validation.error.code.enum';
+import { LogLanguageDictionary } from '../../language/dictionary/language/log.language.dictionary';
+import { LanguageType } from '../../language/dictionary/language/language.type';
 
 export class ValidateMiddleware implements MiddlewareInterface {
-	constructor(private classToValidate: ClassConstructor<object>) {}
-	execute({ method, query, body }: Request, res: Response, next: NextFunction): void {
-		const instance = this.mapToDto(method, query, body);
-		validate(instance).then((errors) => {
-			// Определить сообщение об ошибрах по их ключам.
-			errors.length > 0 ? res.status(400).send(errors) : next();
-		});
+	constructor(
+		private classToValidate: ClassConstructor<object>,
+		private readonly storageService: StorageServiceInterface,
+	) {}
+
+	public async execute(
+		{ method, query, body }: Request,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const lang: LanguageType = await this.storageService.getKeyValue<LanguageType>(
+			AllowedTokenEnum.LANGUAGE,
+		);
+		const dto = await this.mapToDto(method, query, body);
+		const errors = await this.getErrors(dto, lang);
+		// Определить сообщение об ошибрах по их ключам.
+		errors.length > 0 ? res.status(400).send(errors) : next();
 	}
 
-	private mapToDto(method: string, query: any, body: any): object {
+	private async mapToDto(method: string, query: any, body: any): Promise<object> {
 		switch (method) {
 			case 'GET':
 				return plainToInstance(this.classToValidate, query);
@@ -22,5 +37,23 @@ export class ValidateMiddleware implements MiddlewareInterface {
 			default:
 				return plainToInstance(this.classToValidate, null);
 		}
+	}
+
+	private async getErrors(dto: object, language: LanguageType): Promise<ValidationError[]> {
+		const errors = await validate(dto);
+		// Меняем коды ошибок на их реальные значения из словаря.
+		for (const error of errors) {
+			for (const constraint in error.constraints) {
+				switch (error.constraints[constraint]) {
+					case ValidationErrorCodeEnum.LANG_IS_EMPTY_CODE:
+						error.constraints[constraint] = LogLanguageDictionary[language].langIsEmptyMsg;
+						break;
+					default:
+						error.constraints[constraint] = LogLanguageDictionary[language].smtWentWrong;
+				}
+			}
+		}
+
+		return errors;
 	}
 }
